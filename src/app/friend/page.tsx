5d7059a359bf0cef0e10c9606e6a735f1f468e6d
米中拓未 <mumimumi314@gmail.com>
2025-04-30 16:07:30 +0900

    feat(ui):フレンドページ作成

---
+++ src/app/friend/page.tsx
@@ -0,0 +1,11 @@
+import FriendContainer from '@/components/features/friend/FriendContainer';
+import Providers from '@/components/features/providers';
+
+const friend:React.FC =()=>{
+    return(
+        <Providers>
+            <FriendContainer></FriendContainer>
+        </Providers>
+    )
+}
+export default friend;
