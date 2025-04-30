5b75437f7cb2f9bf2ef655f748e9c9f00aeade95
米中拓未 <mumimumi314@gmail.com>
2025-04-30 09:11:46 +0900

    refactor(appRouter): tRPC index を整理

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -2,11 +2,17 @@ import { router } from '@/server/trpc';
 import { authRouter } from './routers/authRouter';
 import { userRouter } from './routers/userRouter';
 import { chatRouter } from './routers/chatRouter';
+import { todoRouter } from './routers/todoRouter';
+import { calendarRouter } from './routers/calendarRouter';
+import { friendRouter } from './routers/friendRouter';
 
 export const appRouter = router({
   auth: authRouter,
   user: userRouter,
   chat: chatRouter,
+  todo: todoRouter,
+  calendar: calendarRouter,
+  friend: friendRouter,
 });
 
 export type AppRouter = typeof appRouter;
