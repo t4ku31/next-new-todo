f942da7f61140101b8233904150edd557a5ef020
米中拓未 <mumimumi314@gmail.com>
2025-05-01 22:12:18 +0900

    refactor(layout):layoutからClientLayout(ヘッダー)を削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import "./globals.css";
 import Providers from "@/components/features/providers";
-import { ClientLayout } from "@/components/ClientLayout";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,9 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <ClientLayout>
             {children}
-          </ClientLayout>
         </Providers>
       </body>
     </html>
