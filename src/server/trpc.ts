abf544c6bfa0558b09f22929a9981d6083b72c19
米中拓未 <mumimumi314@gmail.com>
2025-05-01 19:18:28 +0900

    refactor(trpc):importを修正

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,5 +1,5 @@
 import { initTRPC } from '@trpc/server';
-import type { Context } from './context-http';
+import type { Context } from './context';
 
 const t = initTRPC.context<Context>().create();
 
